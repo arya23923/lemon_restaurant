3bf10cdfb7e9b5559140f9a28d421c9b725fa8d7
Arya Gupta <artagupta239gmail.com>
2024-12-05 01:48:22 +0530

    header portion of website done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,14 +5,17 @@ import './Header.css'
 function Header() {
     return(
         <>
-            <div className="left">
-                <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
-                <p>One of Chicago’s finest and oldest restaurant serving scrumptious and appetizing meals to it’s customers since 1967. Hope you enjoy our service and leave us a good rating! </p>
-            </div>
-            <div className="right">
-                <img src={header_img} />
-            </div>
+            <header>
+                <div className="left">
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>One of Chicago’s finest and oldest restaurant serving scrumptious and appetizing meals to it’s customers since 1967. Hope you enjoy our service and leave us a good rating! </p>
+                    <button>Reserve Table</button>
+                </div>
+                <div className="right">
+                    <img src={header_img} />
+                </div>
+            </header>
         </>
     )
 }
---
+++ src/Components/Main.js
@@ -0,0 +1,5 @@
+import React from "react";
+import './Main.css';
+import taco from '../images/mexican-tacos-with-beef-tomato-sauce-salsa.png';
+import cheesecake from '../images/vecteezy_cheesecake-with_25066829.png'
+import pizza from '../images/vecteezy_pizza-side-view-with_24589239.png'
